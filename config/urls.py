a079897b25f6fb4bcbcf333ff69ccd90fe02fdba
dastonsultonov8747 <158143784+dastonsultonov8747@users.noreply.github.com>
2025-01-15 18:13:35 +0500

    Apply .gitignore rules

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('users/', include('accounts.urls')),
 ]
 
 # Static va Media fayllarni papkadan ulash
